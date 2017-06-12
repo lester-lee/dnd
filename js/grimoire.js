a83f303abc483681f70421897afe3c13f2a36397
Lester Lee <ll6@williams.edu>
2017-06-12 01:54:03 -0400

    nav almost done

---
+++ js/grimoire.js
@@ -0,0 +1,38 @@
+function toggleFilter(text){
+  console.log(text);
+}
+
+function clearFilters(){
+  console.log("clear filter");
+}
+
+$(document).ready(function() {
+  $('.toggle-filters').on('click', function() {
+    btn = $(this);
+    if (btn.attr('data-toggled') == 0) {
+      btn.attr('data-toggled', 1);
+      $('.spell-filters').removeClass('filters-hide');
+      $('.spell-filters').addClass('filters-show');
+      btn.removeClass('glyphicon-chevron-down');
+      btn.addClass('glyphicon-chevron-up');
+    } else {
+      btn.attr('data-toggled', 0);
+      $('.spell-filters').removeClass('filters-show');
+      $('.spell-filters').addClass('filters-hide');
+      btn.addClass('glyphicon-chevron-down');
+      btn.removeClass('glyphicon-chevron-up');
+    }
+  });
+
+  $('.spell-filter').on('click', function() {
+    f = $(this);
+    f.toggleClass('active-filter');
+    toggleFilter(f.attr('data-tags'));
+  });
+
+  $('.clear-filters').on('click', function() {
+    $(this).siblings().removeClass('active-filter');
+    clearFilters();
+  });
+
+});
