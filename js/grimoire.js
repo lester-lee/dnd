8bd39f5aaeb8254cdb8ec8e383effb07a382e27a
Lester Lee <ll6@williams.edu>
2018-07-13 22:04:35 -0400

    Start refactoring grimoire.js

--- js/grimoire.js
+++ js/grimoire.js
@@ -1,26 +1,28 @@
 var filters = [];
 
 function refreshList(text) {
-  var li, i, tags, show;
+  var spells, i, tags;
   tfilters = text || filters;
   var fi, flen = tfilters.length;
-  // console.log(tfilters);
-  li = $('#spell-container').children();
-  for (i = 0; i < li.length; i++) {
-    show = true;
-    tags = li[i].getAttribute("data-tags").toUpperCase();
+  spells = document.querySelectorAll(".Card");
+  for (i = 0; i < spells.length; i++) {
+    spell = spells[i];
+    tags = spell.getAttribute("data-tags").toUpperCase();
     for (fi = 0; fi < flen; fi++) {
-      // console.log(tfilters[fi], tags);
-      if (tags.indexOf(tfilters[fi]) < 0) {
-        show = false;
-        break;
+      if (tags.indexOf(tfilters[fi]) <= 0) {
+        spell.classList.add('is-hidden');
+      }else {
+        spell.classList.remove('is-hidden');
       }
     }
-    li[i].style.display = show ? "" : "none";
   }
 }
 
 function toggleFilter(text) {
+  /* Look for text in filters[]
+   * If exists, remove; otherwise, add.
+   */
+
   var filter, list, li, i, tags, idx;
   filter = text.toUpperCase();
   idx = filters.indexOf(filter);
@@ -39,40 +41,23 @@ function clearFilters() {
 
 $(document).ready(function() {
 
-  $('.toggle-filters').on('click', function() {
-    btn = $(this);
-    if (btn.attr('data-toggled') == 0) {
-      btn.attr('data-toggled', 1);
-      $('.spell-filters').removeClass('filters-hide');
-      $('.spell-filters').addClass('filters-show');
-      btn.removeClass('glyphicon-chevron-down');
-      btn.addClass('glyphicon-chevron-up');
-    } else {
-      btn.attr('data-toggled', 0);
-      $('.spell-filters').removeClass('filters-show');
-      $('.spell-filters').addClass('filters-hide');
-      btn.addClass('glyphicon-chevron-down');
-      btn.removeClass('glyphicon-chevron-up');
-    }
+  $('.Masthead-toggle').on('click', function() {
+    $(this).toggleClass('is-active');
   });
 
-  $('.spell-filter').on('click', function() {
-    f = $(this);
-    f.toggleClass('active-filter');
-    toggleFilter(f.attr('data-tags'));
-  });
-
-  $('.clear-filters').on('click', function() {
-    $(this).siblings().removeClass('active-filter');
+  $('.js-clear-filter').on('click', function () {
+    $(this).siblings().removeClass('is-active');
     clearFilters();
   });
 
-  $('.spell-name').on('click', function() {
-    $(this).parent().toggleClass('show-spell');
+  $('.js-filter').on('click', function() {
+    f = $(this);
+    f.toggleClass('is-active');
+    toggleFilter(f.attr('data-tag'));
   });
 
-  $('div').on('mousewheel DOMMouseScroll', function(e) {
-
+  $('.Card-desc').on('mousewheel DOMMouseScroll', function(e) {
+    // prevent scrolling card from scrolling body
     var e0 = e.originalEvent;
     var delta = e0.wheelDelta || -e0.detail;
 
@@ -80,7 +65,7 @@ $(document).ready(function() {
     e.preventDefault();
   });
 
-  $('.navbar-search').on('keyup', function() {
+  $('.Masthead-search').on('keyup', function() {
     var filter = $(this)[0].value.toUpperCase().split(",");
     refreshList(filter);
   });
